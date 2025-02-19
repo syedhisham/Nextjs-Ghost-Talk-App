c6e6457ae91b52c296b6661359aa691bc559abaa
Syed Hisham Shah <syedhishamshah27@gmail.com>
2025-02-20 02:09:51 +0500

    add: validation schemas and user model for authentication and messaging

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const messageSchema = z.object(
+    {
+        content: z
+        .string()
+        .min(10, {message: "Conent must be atleast 10 characters"})
+        .max(300, {message: "Content must not be more than 300 characters"})
+    }
+)
