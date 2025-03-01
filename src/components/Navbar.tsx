599661a1a045e0610fdc2187ce5e8c06e9aa946b
Syed Hisham Shah <syedhishamshah27@gmail.com>
2025-03-02 00:44:17 +0500

    add: implement Navbar component with user session handling and navigation links

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { signOut, useSession } from "next-auth/react"
+import Link from "next/link"
+import {User} from 'next-auth'
+import { Button } from "./ui/button"
+const Navbar = () => {
+
+    const {data: session} = useSession();
+    const user: User = session?.user as User
+  return (
+    <nav className="p-4 md:p-6 shadow-md">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+            <a href="#">Ghost Talk</a>
+            {
+                session ? (
+                    <div>
+                        <span className="mr-4">Welcome {user?.username || user?.email}</span>
+                    <Button onClick={() => signOut()} className="w-full md:w-auto">Logout</Button>
+                    </div>
+                ) : (
+                    <Link href="/sign-in">
+                        <Button className="w-full md:w-auto">Login</Button>
+                    </Link>
+                )
+            }
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
